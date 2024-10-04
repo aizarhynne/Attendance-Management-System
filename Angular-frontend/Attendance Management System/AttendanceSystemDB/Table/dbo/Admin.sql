77eacf6cd4170d76c2086416389d0b9f159689d0
aizarhynne <aizarhynne90@gmail.com>
2024-10-04 15:10:37 +0400

    Added both Angular frontend and .NET backend

---
+++ Angular-frontend/Attendance Management System/AttendanceSystemDB/Table/dbo/Admin.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[Admin]
+(
+	admin_id INT PRIMARY KEY IDENTITY(1,1),
+    admin_name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    password VARCHAR(100) NOT NULL,
+    created_at DATETIME DEFAULT GETDATE()
+   
+
+)
+
+
+
---
+++ backend-api/Attendance Management System/Attendance Management System/Model/User.cs
@@ -0,0 +1,21 @@
+﻿
+namespace Attendance_Management_System.Models
+{
+    public class User  
+    {
+        public int UserId { get; set; }
+
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Department { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+
+        public byte[]? Photo { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public DateTime? UpdatedAt { get; set; }
+
+        public ICollection<Attendance>? AttendanceRecords { get; set; }
+    }
+}
