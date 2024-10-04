39c1e85bd727773df1c88b75e84526f7022dd25d
aizarhynne <aizarhynne90@gmail.com>
2024-10-04 15:10:37 +0400

    Added both Angular frontend and .NET backend

---
+++ Angular-frontend/Attendance Management System/Attendance Management System/Controllers/AdminController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using Attendance_Management_System.Models;
+
+namespace Attendance_Management_System.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AdminController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<Admin>> GetAdmins()
+        {
+            return new List<Admin>
+            {
+                new Admin { AdminId = 1, AdminName = "SuperAdmin", Email = "admin@example.com" }
+            };
+        }
+
+        [HttpPost]
+        public ActionResult<Admin> PostAdmin(Admin admin)
+        {
+            return CreatedAtAction(nameof(GetAdmins), new { id = admin.AdminId }, admin);
+        }
+    }
+}
